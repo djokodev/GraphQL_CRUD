4aaa274693e67b2bf00a3749031ce7d627799206
M@teo22 <djokodev75@gmail.com>
2024-07-10 16:04:34 +0200

    end project

--- blog/type.py
+++ blog/type.py
@@ -1,5 +1,19 @@
 from graphene_django import DjangoObjectType
 from blog.models import Author, Post
+from graphene import relay
+
+
+class PostNode(DjangoObjectType):
+    class Meta:
+        model = Post
+        filter_fields = ['title', 'content']
+        interfaces = (relay.Node, )
+
+class AuthorNode(DjangoObjectType):
+    class Meta:
+        model = Author
+        filter_fields = ['name']
+        interfaces = (relay.Node, )
 
 
 class PostType(DjangoObjectType):
